97e8421be7d54f78158bd6a342f5e351e5ba7891
Sergey Ponomarenko <sergey@luckyteam.co.uk>
2022-03-29 14:03:18 +0300

    fix github workflow test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+const config = {
+  bail: true,
+  verbose: true,
   preset: '@vue/cli-plugin-unit-jest',
-  "transform": {
+  transform: {
     "^.+\\.vue$": "@vue/vue3-jest"
   }
-}
+};
+
+module.exports = config;
